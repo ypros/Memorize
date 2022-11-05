64aa9e57f60084ed86196e6a16b20b07c9f85003
Yuriy Prosvirnin <prosvirniny@gmail.com>
2022-11-05 18:26:07 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by YURY PROSVIRNIN on 05.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
